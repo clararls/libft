9e0668bc755575083ceec778ad165c0d93c4ec02
Clara Rojano León-Salas <clararojanols@gmail.com>
2022-05-03 11:07:11 +0200

    hasta strrchr

--- Makefile
+++ Makefile
@@ -18,7 +18,7 @@ CFLAGS		= -Wall -Wextra -Werror -I.
 
 SRCS		=	ft_isalpha.c ft_isdigit.c ft_isalnum.c ft_isascii.c ft_isprint.c \
 				ft_strlen.c ft_memset.c ft_bzero.c ft_memcpy.c ft_memmove.c ft_strlcpy.c \
-				ft_strlcat.c   
+				ft_strlcat.c ft_toupper.c ft_tolower.c ft_strchr.c ft_strrchr.c
 
 OBJS		= $(SRCS:.c=.o)
 
@@ -29,7 +29,7 @@ $(NAME):	$(OBJS)
 	ar rcs $(NAME) $(OBJS)
 
 clean:
-	@rm -f $(OBJS) $(BONUS_OBJS)
+	@rm -f $(OBJS)
 	@echo "Ya no hay .o"
 
 fclean:			clean
--- libft.h
+++ libft.h
@@ -17,15 +17,19 @@
 
 int				ft_isalpha(int c);
 int				ft_isdigit(int c);
-int			    ft_isalnum(int c);
+int				ft_isalnum(int c);
 int				ft_isascii(int c);
 int				ft_isprint(int c);
-int	    		ft_strlen(const char *s);
+int				ft_strlen(const char *s);
 void			*ft_memset(void *b, int c, size_t len);
 void			ft_bzero(void *s, size_t n);
 void			*ft_memcpy(void *dst, const void *src, size_t n);
 void			*ft_memmove(void *dst, const void *src, size_t len);
 unsigned int	ft_strlcpy(char *dst, const char *src, unsigned int size);
 unsigned int	ft_strlcat(char *dst, const char *src, unsigned int size);
+int				ft_toupper(int c);
+int				ft_tolower(int c);
+char			*ft_strchr(const char *s, int c);
+char			*ft_strrchr(const char *s, int c);
 
 #endif
